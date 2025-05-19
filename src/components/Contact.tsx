437bc543cc71f4b07a62008f9ff835da355a2d14
kttkgn <41293141+kttkgn@users.noreply.github.com>
2025-05-19 23:35:12 +0800

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -22,7 +22,7 @@ export default function Contact() {
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
-                    <span className="text-gray-600">北京市朝阳区</span>
+                    <span className="text-gray-600">北京市丰台区泥洼北路6号院9号楼203-354室</span>
                   </div>
                   <div className="flex items-center space-x-4">
                     <svg className="w-6 h-6 text-blue-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
