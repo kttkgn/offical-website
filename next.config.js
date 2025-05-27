85eb8956cf813fe5289b03b37fcadfa2983c5577
lyggo <otakzliu@163.com>
2025-05-27 17:44:26 +0800

    add gzip

--- next.config.js
+++ next.config.js
@@ -10,6 +10,40 @@ const nextConfig = {
   compress: true,
   reactStrictMode: true,
   swcMinify: true,
+  productionBrowserSourceMaps: false,
+  experimental: {
+    h2ServerPush: true,
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig; 
